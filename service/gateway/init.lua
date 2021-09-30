26ce1a4da7a17d78f778bf3bd97013c61b9c795e
jidibinlin <2694273649@qq.com>
2021-09-30 09:16:45 +0800

    factor

--- service/gateway/init.lua
+++ service/gateway/init.lua
@@ -1,8 +1,60 @@
 local skynet = require "skynet"
+local serviceConfig = require "serviceConfig"
 local s = require "service"
 
+function conn()
+   local m ={
+      fd=nil,
+      playerId = nil
+   }
+   return m
+end
+
+function gatePlayer()
+   local m ={
+      playerid = nil,
+      agent = nil,
+      con = nil,
+   }
+   return m
+end
+
+local recv_loop = function (fd)
+   socket.start(fd)
+   skynet.error("socket connected"..fd)
+   local readbuff = ""
+   while true do
+      local recvstr = socket.read(fd)
+      if recvstr then
+         readbuff = readbuff..recvstr
+         readbuff = process_buff(fd,readbuff)
+      else
+         skynet.error("skynet close"..fd)
+         disconnect(fd)
+         socket.close(fd)
+         return
+      end
+   end
+   end
+
+
+   local connect =  function(fd,adrr)
+   print("connect from"..addr.." "..fd)
+   local c = conn()
+   conns[fd]=c
+   c.fd=fd
+   skynet.fork(recv_loop,fd)
+end
+
+
 function s.init()
-   skynet.error("[start]"..s.name.." "..s.id)
+   local node = skynet.getenv("node")
+   local nodecfg = serviceConfig[node]
+
+   local port = nodecfg.gateway[s.id].port
+   local listenfd = socket.listen("0.0.0.0",port)
+   skynet.error("Listening socket: ","0.0.0.0",port)
+   socket.start(listenfd,connect)
 end
 
 s.start(...)
