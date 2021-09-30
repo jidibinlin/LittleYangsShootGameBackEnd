0d6100def4f792989a5dc96d3606099faf116d07
jidibinlin <2694273649@qq.com>
2021-09-30 21:18:59 +0800

    bug to fix

---
+++ proto/login.pb
@@ -0,0 +1,11 @@
+
+µ
+login.protologin"Q
+Login
+cmd (	Rcmd
+playerid (	Rplayerid
+password (	Rpassword"D
+Resp
+cmd (	Rcmd
+stat (Rstat
+reason (	Rreasonbproto3
--- service/gateway/init.lua
+++ service/gateway/init.lua
@@ -1,6 +1,19 @@
 local skynet = require "skynet"
 local serviceConfig = require "serviceConfig"
 local s = require "service"
+local pb=require "pb"
+local protoc = require "protoc"
+local socket = require "skynet.socket"
+
+--local proto3 = true
+-- protobuf
+local pc = protoc.new()
+pc:addpath(skynet.getenv("proto_path"))
+-- login relate
+pc:loadfile('login.pb')
+conns = {} -- [fd]=conn
+players = {} -- [playerid] = gateplayer
+
 
 function conn()
    local m ={
@@ -19,15 +32,116 @@ function gatePlayer()
    return m
 end
 
+local msg_unpack = function(recv)
+   local msg = assert(pb.decode('login.'..recv.cmd),recv)
+   --Debug
+   print("msg unpack:"..table.concat(msg,","))
+   return msg.cmd,msg
+end
+
+local msg_pack = function (msg)
+   local type = msg.cmd
+   local stat,size = pb.load("login."..type)
+   local buff = string.pack(">h",size)
+   buff = buff..pb.encode("login."..type,msg)
+   return buff
+end
+
+s.resp.send_by_fd = function (source,fd,msg)
+   if not conns[fd] then
+      return
+   end
+   local buff = msg_pack(msg)
+   skynet.error("send "..fd.." {"..table.concat(msg,",").."}")
+   socket.write(fd,buff)
+end
+
+s.resp.send = function (source,playerid,msg)
+   local gplayer= players[playerid]
+   if gplayer ==nil then
+      return
+   end
+   local c = gplayer.conn
+   if c==nil then
+      return
+   end
+   s.resp.send_by_fd(nil,c.fd,msg)
+end
+
+s.resp.sure_agent = function (source,fd,playerid,agent)
+   local conn = conns[fd]
+   if not conn then
+      skynet.call("agentmgr","lua","reqkick",playerid,"未完成登录即下线")
+      return false
+   end
+   conn.playerid = playerid
+   local gplayer = gateplayer()
+   gplayer.playerid = playerid
+   gplayer.agent = agent
+   gplayer.conn = conn
+   players[playerid]=gplayer
+   return true
+end
+
+local disconnect = function (fd)
+   local c = conns[fd]
+   if not c then
+      return
+   end
+
+   local playerid = c.playerid
+   if not playerid then
+      return
+   else
+      players[playerid]=nil
+      local reason = "fall down"
+      skynet.call("agentmgr","lua","reqkick",playerid,reason)
+
+   end
+end
+
+s.resp.kick = function (source,playerid)
+   local gplayer = players[playerid]
+   if not gplayer then
+      return
+   end
+   local c= gplayer.conn
+   players[playerid] = nil
+   if not c then
+      return
+   end
+   conns[c.fd]=nil
+   disconnect(c.fd)
+   socket.close(c.fd)
+end
+
+local process_msg = function (fd,msgblock)
+   local cmd,msg = msg_unpack(msgblock)
+   skynet.error("recv ".."{"..table.concat(msg,",").."}")
+   local conn = conns[fd]
+   local playerid = conn.playerid
+
+   if not playerid then
+      local node = skynet.getenv("node")
+      local nodecfg = serviceConfig[node]
+      local loginid = math.random(1,#serviceConfig.login)
+      local login = "login"..loginid
+      skynet.send(login,"lua","client",fd,cmd,msg)
+   else
+      local gplayer = players[playerid]
+      local agent = gplayer.agent
+      skynet.send(agent,"lua","client",cmd,msg)
+   end
+end
+
 local recv_loop = function (fd)
    socket.start(fd)
    skynet.error("socket connected"..fd)
-   local readbuff = ""
    while true do
-      local recvstr = socket.read(fd)
-      if recvstr then
-         readbuff = readbuff..recvstr
-         readbuff = process_buff(fd,readbuff)
+      local num = socket.read(fd,2)
+      local recv = socket.read(fd,num)
+      if recv then
+         process_msg(fd,recv)
       else
          skynet.error("skynet close"..fd)
          disconnect(fd)
@@ -35,17 +149,16 @@ local recv_loop = function (fd)
          return
       end
    end
-   end
+end
 
 
-   local connect =  function(fd,adrr)
+local connect =  function(fd,adrr)
    print("connect from"..addr.." "..fd)
    local c = conn()
    conns[fd]=c
    c.fd=fd
    skynet.fork(recv_loop,fd)
 end
-
 
 function s.init()
    local node = skynet.getenv("node")
