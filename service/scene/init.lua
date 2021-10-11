c853d0f69f780db21707c4a72dd84793c3de9bea
jidibinlin <2694273649@qq.com>
2021-10-11 15:36:03 +0800

    combine frame way

--- service/scene/init.lua
+++ service/scene/init.lua
@@ -1,9 +1,12 @@
 local skynet = require "skynet"
 local serviceConfig = require "serviceConfig"
+local idToName = require "protoConfig"
 local s = require "service"
 
 players={}
 local playerNum=0
+
+local key = true;
 
 local function player()
    local m = {
@@ -16,13 +19,31 @@ local function player()
 end
 
 local function broadcast(frame)
-   --skynet.error("广播")
-   for index, player in ipairs(players) do
-      local msg = table.remove(player.msgquee,1)
-      local frameMsg = {cmd="frame",frame=frame}
-      s.send(player.node,player.agent,"send",frameMsg)
+   while key == false do
+   end
+   key = false
+   skynet.error("广播")
+   local msg = {}
+   msg.id = 7
+   --msg.cmd =idToName[msg.id]
+   msg.cmd = "broadcastStoC"
+   msg.frame = frame
+   msg.frames = {}
+
+   for index, player in pairs(players) do
+      skynet.error("broadcastStoC insert",player.playerid)
+      local msgQuee = player.msgquee
+      local frame = table.remove(msgQuee)
+      table.insert(msg.frames,frame)
+      --msg.frames[player.playerid]=frame
+   end
+   key = true
+   skynet.error("broadcastStoC frame number     "..#msg.frames)
+
+   for index, player in pairs(players) do
       s.send(player.node,player.agent,"send",msg)
    end
+
 end
 
 local function update(frame)
@@ -36,9 +57,12 @@ local startGame = function ()
    local playerIds = {}
 
    for _, player in pairs(players) do
-      print("insert player id ",player.id)
+      print("insert player id ",player.playerid)
       table.insert(playerIds,player.playerid)
    end
+   -- for key, value in pairs(playerIds) do
+   --    print(value)
+   -- end
 
    for _, player in pairs(players) do
       s.send(player.node,player.agent,"send",{id=5,cmd="startGame",players=playerIds})
@@ -49,13 +73,16 @@ local startGame = function ()
 
          while true do
             frame = frame+1
+            --local beforeBroad = skynet.now()
             local isok,err = pcall(update,frame)
+            --skynet.error("broad consume",beforeBroad,(skynet.now()))
 
             if not isok then
                skynet.error(err)
             end
+            skynet.error("current frame",frame)
             local etime = skynet.now()
-            local waittime = frame*2-(etime-stime)
+            local waittime = frame*20-(etime-stime)
             if waittime <=0 then
                waittime = 1
             end
@@ -71,15 +98,25 @@ s.resp.enterScene = function (source,playerid,node,agent)
       return false
    end
    local player = player()
-   player.id = playerid
+   player.playerid = playerid
    player.node = node
    player.agent = agent
    playerNum = playerNum+1;
-   players[player.id]=player
+   players[player.playerid]=player
 
    if playerNum == 2 then
       startGame()
    end
+end
+
+s.resp.broadcastCtoS = function(source,playerid,msg)
+   --skynet.error("broadcastCtoS")
+   while key == false do
+   end
+
+   key = false
+   table.insert(players[playerid].msgquee,(msg.frame))
+   key = true
 end
 
 -- TODO sync time with players
